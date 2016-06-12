4766cce6b1b24afa025d927cb391aa196e1b07b7
zeriet woldeab <zer.woldeab@gmail.com>
2016-06-11 17:46:02 -0700

    Additional datatructures and algoritm excercies

--- DataStructuresAndAlgorithms/src/com/zorolabs/stringMisc/RemoveDuplicates.java
+++ DataStructuresAndAlgorithms/src/com/zorolabs/stringMisc/RemoveDuplicates.java
@@ -1,46 +1,43 @@
 package com.zorolabs.stringMisc;
 
 public class RemoveDuplicates {
-	
-	
-public static void main(String[] args){
-	
-	String temp = "ddaalamera";
-	char[] str = temp.toCharArray();
-	removeDuplicates(str);
-	System.out.println(str);
-	
-//	String stt = "2.0.3";
-//	System.out.println(stt.indexOf('.'));
-	
-	
-	
-}
 
-public static void removeDuplicates(char[] str){
-	
-	if(str == null)
-		return;
-	int len = str.length;
-	if (len <2) return;
-	
-	int tail= 1;
-	
-	for(int i=1;i<len;++i){
-		int j;
-		for(j=0;j<tail;++j){
-			if(str[i] ==str[j])
-				break;
-		}
-		if (j == tail) {
-			str[tail] = str[i];
-			++tail;
-		}
-		
+	public static void main(String[] args) {
+
+		String temp = "ddaalamera";
+		char[] str = temp.toCharArray();
+		removeDuplicates(str);
+		System.out.println(str);
+
+		// String stt = "2.0.3";
+		// System.out.println(stt.indexOf('.'));
+
 	}
-	str[tail] =0;
-	
-	
-}
+
+	public static void removeDuplicates(char[] str) {
+
+		if (str == null)
+			return;
+		int len = str.length;
+		if (len < 2)
+			return;
+
+		int tail = 1;
+
+		for (int i = 1; i < len; ++i) {
+			int j;
+			for (j = 0; j < tail; ++j) {
+				if (str[i] == str[j])
+					break;
+			}
+			if (j == tail) {
+				str[tail] = str[i];
+				++tail;
+			}
+
+		}
+		str[tail] = 0;
+
+	}
 
 }
